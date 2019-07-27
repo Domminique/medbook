1a763465938752777c6c719af4bc4ee3041f2019
domminique <imbugad@gmail.com>
2019-07-27 12:59:22 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'medbook';
+
+  dishes =  [
+    {
+    name: 'Salad', choices: [
+    { name: 'Santa Fe' },
+    { name: 'Greek' },
+    { name: 'Asian' },
+    ],
+    related: [
+    {
+    name: 'Dressing', choices: [
+    { name: 'Italian' },
+    { name: 'Blue Cheese' },
+    { name: 'Ranch' },
+    ]
+    },
+    {
+    name: 'Bread', choices: [
+    { name: 'Italian' },
+    { name: 'Flat' },
+    { name: 'Sourdough' },
+    ]
+    }
+    ]
+    },
+    {
+    name: 'Entree', choices: [
+    { name: 'Steak' },
+    { name: 'Salmon' },
+    { name: 'Rice' },
+    ],
+    related: [
+    ]
+    },
+    {
+    name: 'Soup', choices: [
+    { name: 'Minestrone' },
+    { name: 'Hot and sour' },
+    { name: 'Miso' },
+    ],
+    related: [
+    {
+    name: 'Bread', choices: [
+    { name: 'Breadsticks'}
+    ]
+    }
+    ]
+    }
+    ];
+    
 }
