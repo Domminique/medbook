f0a2cd4dbeb471cc9c2d08537d9d4f028f912665
domminique <imbugad@gmail.com>
2019-07-27 14:53:29 +0300

    add *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'medbook';
   checked : boolean;
+  show: boolean = true;
   dishes =  [
     {
     name: 'Salad', 
